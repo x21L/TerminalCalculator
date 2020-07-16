c813bfc459019f0807b5f963babc46d1155c5a16
Lukas Wais <lukas.wais@outlook.de>
2020-07-16 12:47:53 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    /*
+        your code comes here...
+     */
+
+    public static void main(String[] args) {
+        boolean terminate = false;
+        do {
+            /*
+                 your code comes here
+            */
+        } while (!terminate);
+    }
+}
